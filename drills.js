c0951d42ddcc5d603e660ba94f3e9841563a54f6
Jess Doyle <jessica.e.doyle@gmail.com>
2019-09-30 10:57:18 -0400

    added powerCalculatorRec

--- drills.js
+++ drills.js
@@ -18,3 +18,25 @@ function countSheep(n) {
         countSheep(n-1)
     }
 }
+
+
+// 2. Power Calculator
+// Write a function called powerCalculator() that takes two parameters, an integer as a base, and another integer as an exponent. The function returns the value of the base raised to the power of the exponent. Use only exponents greater than or equal to 0 (positive numbers)
+
+// powerCalculatorRec(10,2) should return 100
+// powerCalculatorRec(10,-2) should return exponent should be >= 0
+
+function powerCalculatorRec(base, exponent) {
+    if (exponent < 0) {
+        return 'exponent should be >= 0'
+    } else if (base < 0) {
+        return 'base should be >= 0'
+    } else if (exponent === 0) {
+        return 1;
+    } else if (exponent === 1) {
+        return base;
+    } else {
+        return (base * (powerCalculatorRec(base, exponent-1)))
+    }
+}
+
